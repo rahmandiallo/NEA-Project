7f2bf2d239924662b7ee798c9e8949bf0c5f2fa6
rahmandiallo <112436778+rahmandiallo@users.noreply.github.com>
2023-02-03 14:19:50 +0000

    Update football.py

--- football.py
+++ football.py
@@ -1,1 +1,11 @@
+#football.py code
+import sys
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+import pygame
 
+#from x.x import x
+
+
+if __name__ == '__main__':
+    print('hello world')
